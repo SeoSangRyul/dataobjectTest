a705436c7f1dc1c04ddd837142c393431f35036e
Hagenti <gogumach82@gmail.com>
2018-02-14 10:51:05 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import kr.co.rayworld.dataobject.Schedule;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("test");
+        Schedule s1 = new Schedule();
+        s1.date = "1";
+        System.out.println(s1.date);
+
+    }
+}
---
+++ src/main/java/kr/co/rayworld/dataobject/Schedule.java
@@ -0,0 +1,11 @@
+package kr.co.rayworld.dataobject;
+
+import java.io.Serializable;
+
+public class Schedule implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    public String date;
+    public String time;
+
+}
